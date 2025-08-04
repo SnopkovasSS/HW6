2cbd303025ae2e9a48f13c1f53e8b992ed994eba
SnopkovaSS <rozhkova80@bk.ru>
2025-08-04 16:58:07 +0300

    ДЗ № 6

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,5 +13,91 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        System.out.println("Задание 1-3");
+        int[] intArray = new int[]{1, 2, 3};
+        double[] doubleArray = {1.57, 7.654, 9.986};
+
+
+        printIntArray(intArray);
+        printDoubleArray(doubleArray);
+
+
+        printIntArrayReverse(intArray);
+        printDoubleArrayReverse(doubleArray);
+    }
+
+    public static void printIntArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printDoubleArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printIntArrayReverse(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printDoubleArrayReverse(double[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Задание 4");
+        int[] arr1 = new int[]{1, 2, 3};
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] % 2 != 0) {
+                arr1[i] += 1;
+                System.out.println("Все нечётные числа стали чётными:");
+                System.out.println(Arrays.toString(arr1));
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
