6760fa96fc39e1f60196aa3d6bb2a2b02a8d4884
SnopkovaSS <rozhkova80@bk.ru>
2025-08-04 17:25:55 +0300

    ДЗ6

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,73 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
         }
+        int[] intArray = new int[]{1, 2, 3};
+        double[] doubleArray = {1.57, 7.654, 9.986};
+
+        printIntArray(intArray);
+        printDoubleArray(doubleArray);
+
+        printIntArrayReverse(intArray);
+        printDoubleArrayReverse(doubleArray);
+
+        System.out.println("Задание 4");
+
+        Main1.processArray();
+    }
+
+    public static void printIntArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printDoubleArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printIntArrayReverse(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printDoubleArrayReverse(double[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+}
+
+class Main1 {
+    public static void processArray() {
+        int[] arr = new int[]{1, 2, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] += 1;
+            }
+        }
+
+        System.out.println(java.util.Arrays.toString(arr));
     }
 }
